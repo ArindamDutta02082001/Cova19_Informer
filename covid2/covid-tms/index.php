19760da46d69c49ca37a34cd1b6e6bcd1c8ee7bd
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2023-07-22 01:38:21 +0530

    all files + zip + rar

---
+++ covid2/covid-tms/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">COVA19_INFORMER</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Covid-19 Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white" >
+    <div class="container text-center" style="height:3vh" >
+      <h1>COVA19_INFORMER</h1>
+      <p class="lead">COVID19 - Testing Management System</p>
+    </div>
+  </header>
+
+
+
+  <div id="carouselExampleControls" class="carousel slide" style="width:90vw,height:50vh" data-ride="carousel"  interval="2000" wrap="true">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./img/b3.jpg"  style="width:90vw,height:50vh" alt="First slide" class="active">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/b2.jpg" style="width:90vw,height:50vh" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./img/b1.jpg" style="width:90vw,height:50vh" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About this page</h2>
+          <p class="lead">Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19, virus will experience mild to moderate, respiratory illness & recover without requiring special treatment. Older people and those with underlying medical problem like cardiovascular disease.</p>
+          <p class="lead">The COVID-19 virus spread primarily through droplet of saliva or discharge from the nose when an infected person coughs or sneezes so it’s important that you also practice respiratory etiquette.</p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>Hight Fever 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+             <hr />
+<p><strong>Dry Cough 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+          <hr />
+<p><strong>Shortness of breath!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Wash your Hands often</li>
+            <li>Wear A Face mask</li>
+            <li>Avoid contact with sick people</li>
+            <li>Always cover your cough or sneeze</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
